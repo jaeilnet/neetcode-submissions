8b610bccb89737d2548005b28757bcaa55299b7b
jaeil.joo <sgsgself@naver.com>
2026-07-25 20:18:43 +0900

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.js
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -5,18 +5,19 @@ class Solution {
      * @return {boolean}
      */
     isAnagram(s, t) {
-        if(s.length !== t.length) {
-            return false
+        if (s.length !== t.length) {
+            return false;
         }
 
         const map = {}
 
-        for(let i=0; i<s.length; i++) {
-            map[s[i]] = (map[s[i]] || 0) + 1
-            map[t[i]] = (map[t[i]] || 0) - 1
+        for (let i = 0; i < s.length; i++) {
+            map[s[i]] = (map[s[i]] || 0) + 1;
+            map[t[i]] = (map[t[i]] || 0) - 1;
         }
 
-        for (const key in map) {
+
+        for(const key in map) {
             if(map[key] !== 0) {
                 return false
             }
