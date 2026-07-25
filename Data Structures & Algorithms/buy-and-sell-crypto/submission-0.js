91dd2bdbc9fff29935efea40d006770d496cdc59
jaeil.joo <sgsgself@naver.com>
2026-07-25 21:24:48 +0900

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -4,24 +4,23 @@ class Solution {
      * @return {number}
      */
     maxProfit(prices) {
-        if(!prices.length) return prices.length 
+        // 지금까지 확인한 가격 중 가격 낮은 가격
+        let minPrice = Infinity
 
-        let buy = prices[0]
-        let result = 0
+        // 지금까지 만들 수 있었던 최대 이익
+        let maxProfit = 0
 
-        for(let i=0; i<prices.length; i++) {
-            if(buy > prices[i]) {
-                buy = prices[i]
-            }else {
-                const sell = prices[i] - buy 
-                
-                if(sell > result) {
-                    result = sell
-                }
-            }  
+        for(const price of prices) {
+            // 현재 가격이 더 낮다면 새로운 구매 후보 갱신
+            minPrice = Math.min(minPrice, price)
+
+            // 이전까지의 최소 가격에 사고 현재 가격에 판매했을 때의 이익
+            const currentProfit = price - minPrice
+
+            // 기존 최대 이익보다 크다면 갱신
+            maxProfit = Math.max(maxProfit, currentProfit)
         }
 
-        return result
-        
+        return maxProfit
     }
 }
