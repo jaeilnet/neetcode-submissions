05dcc0968d4cbae1c8ad1a18efad71b182467025
jaeil.joo <sgsgself@naver.com>
2026-07-27 13:08:13 +0900

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+
+        const visitied = new Set()
+
+        let current = head
+        
+        while(current !== null) {
+            if(visitied.has(current)) {
+                return true
+            }
+
+            visitied.add(current)
+            current = current.next
+        }
+
+        return false
+    }
+}
