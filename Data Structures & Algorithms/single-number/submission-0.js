e58ee10c4f974c828db34c9beaa4e12b9f784480
jaeil.joo <sgsgself@naver.com>
2026-07-31 13:39:13 +0900

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        const map = new Map()
+
+        for(let i=0; i<nums.length; i++) {
+            if(!map.get(nums[i])) {
+                map.set(nums[i], 1)
+            }else {
+                map.set(nums[i], map.get(nums[i]) +1)
+            }
+        }
+
+        for(const m of map) {
+            const [key ,value] = m
+            if(value === 1) {
+                return key
+            }
+        }
+    }
+}
